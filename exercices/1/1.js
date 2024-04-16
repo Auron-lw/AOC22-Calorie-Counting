381ee3c56f97b76edbce627d80aa14c0bc51c624
Julien Miguel <miguel.julien.pro@gmail.com>
2024-04-16 14:55:48 +0200

    Part 2 - Total des 3 plus gros mangeur
    Signed-off-by: Julien Miguel <miguel.julien.pro@gmail.com>

--- exercices/1/1.js
+++ exercices/1/1.js
@@ -24,8 +24,6 @@ const findLargestSum = (file) => {
   for (let i = 0; i < lutins.length; i++) {
     // 🦁   Trouve les calories en utilisant `.split("\n")`pour mac ou `.split("\r\n")` pour windows dans notre liste
     const calories = lutins[i].split('\n');
-    console.log(calories);
-
     // 🦁   Initialise une variable sum à 0
     let sum = 0;
     // 🦁   Pour chaque calorie (boucle for)
@@ -33,8 +31,6 @@ const findLargestSum = (file) => {
       // 🦁     Ajoute la calorie à la variable sum
       sum += Number(calories[j]);
     }
-    console.log(sum);
-
     // 🦁   Si la variable sum est plus grande que la variable largestSum
     if (sum > largestSum) {
       // 🦁     Mets la variable sum dans la variable largestSum
@@ -45,14 +41,32 @@ const findLargestSum = (file) => {
   return largestSum;
 };
 
+const findSumOfThreeLargest = (file) => {
+  const fileContent = readFileContent(file);
+
+  const lutins = fileContent.split('\n\n');
+
+  const sums = [];
+  for (let i = 0; i < lutins.length; i++) {
+    const calories = lutins[i].split('\n');
+
+    let sum = 0;
+    for (let j = 0; j < calories.length; j++) {
+      sum += Number(calories[j]);
+    }
+    sums.push(sum);
+  }
+  const sorted = sums.sort((a, b) => b - a);
+
+  return sorted[0] + sorted[1] + sorted[2];
+};
+
 export const part1 = (file) => {
   return findLargestSum(file);
 };
 
-// À faire après
 export const part2 = (file) => {
-  // 🦁 Pour la partie 2, utilise la fonction ici
-  return 0;
+  return findSumOfThreeLargest(file);
 };
 
 //Should be 24000 with data-test
@@ -65,4 +79,4 @@ console.log('Test part1', part1('./data.txt'));
 console.log('Test part2', part2('./data-test.txt'));
 
 //Should be 212836 with data
-console.log('Test part2', part2('./data.txt'));
+// console.log('Test part2', part2('./data.txt'));
